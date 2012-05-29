590faaa9b76fbf3106f47bee9dff14c54b8f1e60
Joe Kurleto <joekur.com>
2012-05-28 23:39:12 -0400

    Erica added contact form

---
+++ pages/_contact.html.php
@@ -0,0 +1,13 @@
+<div class="page" id="contact">
+	<div class="content">	
+
+<h3>I'd love to hear from you!</h3>
+
+<p>Email: garciadesign.me 
+<br>Cell: 248-321-5714</p>
+	<table cellspacing="0" cellpadding="0" border="0"><tr><td><iframe width="400" height="490" frameborder="0" src="http://www.foxyform.com/form.php?id=261857&sec_hash=1ef6d601021"></iframe></td></tr><tr><td align="center"><a style="font:8px Arial;color:#5C5C5C;" href="http://www.foxyform.com">foxyform.com</a></td></tr></table>
+
+
+	</div>
+
+</div>
--- pages/_home.html.php
+++ pages/_home.html.php
@@ -19,7 +19,7 @@
 					</div>
 					<div class="photo bottom_left">
 						<div class="photo_wrapper" data-orig_width="250" data-orig_height="160">
-							<img src="/img/About.jpg" class="project">
+							<img src="/img/Erica2.jpg" class="project">
 						</div>
 					</div>
 					<div class="photo bottom_right">
@@ -32,11 +32,18 @@
 			
 			<div class="proj_desc persistent">
 				
-			<h3>Design is everywhere...<br>and I do it all!</h3>
+	
+<p>Hi! My name is Erica Garcia! I received my degree in design, minor in Spanish, and cognate in 
+math and science from Michigan State University. I graduated from the Honors College gaining unique experiences by pursing my interests and taking on additional course work and 
+projects. Some of my experiences include giving presentations to groups of over 150, creating a variety of stop motion animations with 
+both photography and graphics, designing websites and my very own animated game characters. While at MSU I also tried my best to give back whenever I could, teaching a PHD 
+student English, volunteering at the Mid-Michigan cat rescue and in Detroit, and even traveling to Jamaica with 11 other students to work with children ranging from 3-15 years old, helping 
+them both in the classroom and by painting the walls and installing new flooring in their school. 
+That being said, my primary goal in life is to give back to the world. So while the future is 
+never predictable, I know one thing for a fact, I’ll always be working to make a difference whether 
+it is through teaching, helping others or through great design.</p>
 				
-				<p>Branding, graphic, industrial, interactive, motion, print, user interface and web design </p>
 				
-				<p>Interested in working with me? <a href="mailto:garciadesign.me@gmail.com">garciadesign.me@gmail.com</a><br>Phone: 248-321-5714</p>
 			</div>
 		
 		</div>
